00edbda68f6ca863ab97ddb313a8c82d4c00b8e5
Donshu <donshu.apps@gmail.com>
2026-03-25 12:28:45 -0400

    Commit inicial

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@
 # Contacto: donshu.apps@gmail.com
 # ============================================================
 set -euo pipefail
-RED='\033[0;31m'
-GREEN='\033[0;32m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
+BLUE='\033[1;34m'
 NC='\033[0m'
 INSTALL_DIR="/usr/local/bin"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
